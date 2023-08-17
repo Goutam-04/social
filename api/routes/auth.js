e5b6f8e5a164321c24e6d4302926611d50fa9737
Goutam-04 <goutamnayak2804+github1@gmail.com>
2023-08-18 03:51:56 +0530

    register of a user

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,8 +2,24 @@ const express=require('express')
 const router = express.Router();
 // this ROuteer() function creates a middleware where you can make routes using express
 
-const auth=router.get("/",(req,res)=>{
-    res.send("hello there its me")
+const User=require('../models/User')
+
+const auth=router.post("/register",async (req,res)=>{
+    const newUser = await new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password
+    })
+    try{
+        const user=await newUser.save();
+        res.statusCode(200).json(user)
+    }
+    catch
+        (error){console.log(error);}
+    
+
+  
+
 })
 
 module.exports=auth
