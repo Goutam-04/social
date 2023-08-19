53754840f9f99ac50dedaa3751c306926a048d58
Goutam-04 <goutamnayak2804+github1@gmail.com>
2023-08-20 04:54:18 +0530

    add feed and navbar

---
+++ client/src/components/feed/Feed.js
@@ -0,0 +1,20 @@
+import { useContext, useEffect, useState } from "react";
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
+
+export default function Feed({ username }) {
+  const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
+
+
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+      
+      </div>
+    </div>
+  );
+}
--- client/src/components/topbar/Topbar.js
+++ client/src/components/topbar/Topbar.js
@@ -1,17 +1,48 @@
 import "./topbar.css"
-import { Search, Person, Chat, Notifications } from "@material-ui/icons";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
+
 
 
 export const Topbar = () => {
   return (
     <div className="topbarContainer">
-        <div className="topbarLeft"></div>
-        <div className="topbarCenter"></div>
-        <div className="topbarRight"></div>
-
+    <div className="topbarLeft">
+      
+        <span className="logo">Syncsocial</span>
+  
     </div>
+    <div className="topbarCenter">
+      <div className="searchbar">
+        <Search className="searchIcon" />
+        <input
+          placeholder="Search for friend, post or video"
+          className="searchInput"
+        />
+      </div>
+    </div>
+    <div className="topbarRight">
+      <div className="topbarLinks">
+        <span className="topbarLink">Homepage</span>
+        <span className="topbarLink">Timeline</span>
+      </div>
+      <div className="topbarIcons">
+        <div className="topbarIconItem">
+          <Person />
+          <span className="topbarIconBadge">1</span>
+        </div>
+        <div className="topbarIconItem">
+          <Chat />
+          <span className="topbarIconBadge">2</span>
+        </div>
+        <div className="topbarIconItem">
+          <Notifications />
+          <span className="topbarIconBadge">1</span>
+        </div>
+      </div>
+     <img className="topbarImg" src="/assets/person/1.jpeg" />
+      
+     
+    </div>
+  </div>
   )
 }
