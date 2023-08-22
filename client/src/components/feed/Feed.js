181dfd358e2adef3dea66098198d6e9401a601f9
Goutam-04 <goutamnayak2804+github1@gmail.com>
2023-08-22 23:31:27 +0530

    add share

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './feed.css'
 import Share from '../share/Share'
+import Post from '../post/Post'
 
 const Feed = () => {
   return (
     <div className='feed'>
     <div className='feddWrapper'>
       <Share/>
+      <Post/>
     </div>
 
     </div>
